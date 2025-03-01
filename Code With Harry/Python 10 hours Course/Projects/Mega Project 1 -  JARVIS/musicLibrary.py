949677a4bb5980575ee59650d655921b62b3f460
Soumya-Mahata <soumyarijux@gmail.com>
2025-03-02 02:56:37 +0530

    MEGA PROJECT 1: JARVIS

--- Code With Harry/Python 10 hours Course/Projects/Mega Project 1 -  JARVIS/musicLibrary.py
+++ Code With Harry/Python 10 hours Course/Projects/Mega Project 1 -  JARVIS/musicLibrary.py
@@ -3,6 +3,10 @@ music = {
     "kolaveri" : "https://youtu.be/YR12Z8f1Dh8?feature=shared",
     "despacito" : "https://youtu.be/kJQP7kiw5Fk?feature=shared",
     "pasoori" : "https://youtu.be/5Eqb_-j3FDA?feature=shared",
-    "ma lo ma" : "https://youtu.be/zEqqW-USajs?feature=shared",
+    "bangla song" : "https://youtu.be/zEqqW-USajs?feature=shared",
+
 
 }
+# We can use the music dictionary to store the names of songs as keys and their YouTube links as values.
+# This way, we can easily access the link of a song by its name.
+# The music dictionary is used in the ProcesCommand function to play songs based on user input.
