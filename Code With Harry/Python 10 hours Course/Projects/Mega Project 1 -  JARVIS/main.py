e918499adc91abefbd0d271fd16dac3164dd150a
Soumya-Mahata <soumyarijux@gmail.com>
2025-03-12 05:52:28 +0530

    JAVIS is updated with Gemini AI

--- Code With Harry/Python 10 hours Course/Projects/Mega Project 1 -  JARVIS/main.py
+++ Code With Harry/Python 10 hours Course/Projects/Mega Project 1 -  JARVIS/main.py
@@ -4,7 +4,8 @@ import pyttsx3
 import pocketsphinx
 import musicLibrary
 import requests
-from openai import OpenAI
+# from openai import OpenAI
+from google import genai
 from gtts import gTTS
 import pygame
 import os
@@ -12,6 +13,8 @@ import os
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 newsapi = "news_Api_key"
+chatgptapi = "OpenAI_Api_Key"
+geminiapi = "Gemini_Api_Key"
 
 def speak_old(text):
     engine.say(text)
@@ -30,20 +33,24 @@ def speak(text):
     os.remove("temp.mp3")  # Remove the file after playing
 
 def aiprocess(command):
-    client = OpenAI(api_key="OpenAI_Api_Key",)
+    # client = OpenAI(api_key=chatgptapi,)
 
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "You are a virtual assistant named Jarvis, skilled in general tasks like opening websites, playing music, and fetching news."},
-            {
-                "role": "user",
-                "content": command
-            }
-        ]
+    # completion = client.chat.completions.create(
+    #     model="gpt-4o-mini",
+    #     messages=[
+    #         {"role": "system", "content": "You are a virtual assistant named Jarvis, skilled in general tasks like opening websites, playing music, and fetching news."},
+    #         {
+    #             "role": "user",
+    #             "content": command
+    #         }
+    #     ]
+    # )
+    # return (completion.choices[0].message['content'])
+    client = genai.Client(api_key=geminiapi)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents="You are a virtual assistant named Jarvis, skilled in general tasks like opening websites, playing music, and fetching news. You give short response to the command. The command is: " + command
     )
-
-    return (completion.choices[0].message['content'])
+    return (response.text)
 
 def ProcesCommand(command):
     if "open google" in command.lower():
@@ -102,18 +109,25 @@ if __name__ == "__main__":
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source, timeout=2, phrase_time_limit=2)
+                audio = r.listen(source, timeout= 5, phrase_time_limit= 10)
             word = r.recognize_google(audio)
             print(word)
             if ("jarvis" in word.lower()):
-                speak("yeah!")
-                # Listen for command
-                with sr.Microphone() as source:
-                    print("Jarvis Activated...")
-                    audio = r.listen(source)
-                    command = r.recognize_google(audio)
-                    print(f"Processing...{command}")
-                    ProcesCommand(command)
+                speak("yes Sir!")
+                while True:
+                    # Listen for command
+                    with sr.Microphone() as source:
+                        print("Jarvis Activated...")
+                        audio = r.listen(source, timeout= 5, phrase_time_limit= 10)
+                        command = r.recognize_google(audio)
+                        print(f"Processing...{command}")
+                        ProcesCommand(command)
+            else:
+                print("Sorry, I didn't catch that. Please say 'Jarvis' to activate me. JARVIS is not active.")
 
+        except sr.UnknownValueError:
+            print("Sorry, I could not understand you. JARVIS is not active.")
+        except sr.RequestError as e:
+            print(f"Could not request results; {e} JARVIS is not active.")
         except Exception as e:
-            print("Error; {0}".format(e))
+            print(f"An error occurred: {e} JARVIS is not active.")
